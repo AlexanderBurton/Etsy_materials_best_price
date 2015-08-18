aa45a77557a2d8c0cf6c8c99adbf2490eedefdfc
Alexander Burton <alexanderjburton@gmail.com>
2015-08-18 09:30:14 -0400

    Add resource dir

---
+++ resources/etsySopsThatListMaterials.rb
@@ -0,0 +1,3 @@
+shops=["https://www.etsy.com/shop/BubbleGumDish?ref=l2-shopheader-name","https://www.etsy.com/shop/ohzie?ref=l2-shopheader-name","https://www.etsy.com/shop/karinagracejewelry?ref=l2-shopheader-name","https://www.etsy.com/shop/lizzybirdshop?ref=l2-shopheader-name"]
+
+
